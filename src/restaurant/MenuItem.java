4a8d9a528617a14de0148eaec4493bd0b70c44d6
Sumathi <thasel@gmail.com>
2021-05-17 01:11:54 -0500

    commited Studio counting char

---
+++ src/restaurant/MenuItem.java
@@ -0,0 +1,29 @@
+package restaurant;
+import java.time.LocalDateTime;
+
+public class MenuItem {
+    private double price;
+    private String category;
+    private String lastUpdated;
+    private String description;
+
+    public String getCategory() {
+        return category;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public String getLastUpdated() {
+        return lastUpdated;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setCategory(String inputCategory) {this.category = inputCategory;}
+    public void setPrice(double inputPrice) {
+        this.price = inputPrice;
+    }
+    public void setLastUpdated(String inputDate) {this.lastUpdated = inputDate;}
+    public void setDescription(String inputDescription) {this.description = inputDescription;}
+}
